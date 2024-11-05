571b31a55e62813d8dedc5c3dccafa9d0e30bd8f
Victor Schweitz <victorschweitz@hotmail.com>
2024-11-05 18:56:08 +0100

    Props added (I think ...)

--- src/app/components/AccordianItem.jsx
+++ src/app/components/AccordianItem.jsx
@@ -1,9 +1,9 @@
-const AccordianItem = () =>
+const AccordianItem = ({question, answer}) =>
 {
     return (
         <div className="accordian-item">
-            <h2 className="question bg-green-300 h-12 flex align-center justify-center items-center uppercase font-bold letter tracking-wider cursor-pointer">This is a question</h2>
-            <p className="answer flex align-center justify-center items-center text-black">This is an answer</p>
+            <h2 className="question bg-green-300 h-12 flex align-center justify-center items-center uppercase font-bold letter tracking-wider cursor-pointer">{question}</h2>
+            <p className="answer flex align-center justify-center items-center text-black">{answer}</p>
         </div>
     )
 }
