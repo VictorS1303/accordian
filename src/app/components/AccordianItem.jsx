1393ad39aaa5b46b92eaca8f86f1b1452dbe5303
Victor Schweitz <victorschweitz@hotmail.com>
2024-11-05 19:53:20 +0100

    Something new

--- src/app/components/AccordianItem.jsx
+++ src/app/components/AccordianItem.jsx
@@ -1,9 +1,14 @@
-const AccordianItem = ({question, answer, onToggle}) =>
+const AccordianItem = ({question, answer, isOpen, onToggle}) =>
 {
     return (
         <div className="accordian-item">
             <h2 className="question bg-green-300 h-12 flex align-center justify-center items-center uppercase font-bold letter tracking-wider cursor-pointer" onClick={onToggle}>{question}</h2>
-            <p className="answer flex align-center justify-center items-center text-black">{answer}</p>
+            {
+                isOpen &&
+                <div className="accordian-content">
+                    <p className="answer flex align-center justify-center items-center text-black">{answer}</p>
+                </div>
+            }
         </div>
     )
 }
