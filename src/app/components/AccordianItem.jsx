d2210ac998de8a3245b8ff344677601df4c9b599
Victor Schweitz <victorschweitz@hotmail.com>
2024-11-05 19:22:24 +0100

    Added onToggle prop

--- src/app/components/AccordianItem.jsx
+++ src/app/components/AccordianItem.jsx
@@ -1,8 +1,8 @@
-const AccordianItem = ({question, answer}) =>
+const AccordianItem = ({question, answer, onToggle}) =>
 {
     return (
         <div className="accordian-item">
-            <h2 className="question bg-green-300 h-12 flex align-center justify-center items-center uppercase font-bold letter tracking-wider cursor-pointer">{question}</h2>
+            <h2 className="question bg-green-300 h-12 flex align-center justify-center items-center uppercase font-bold letter tracking-wider cursor-pointer" onClick={onToggle}>{question}</h2>
             <p className="answer flex align-center justify-center items-center text-black">{answer}</p>
         </div>
     )
